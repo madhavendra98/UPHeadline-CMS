c12e83911e906bc9988d212c775ba18c7bd03607
madhavendra98 <smartmadhavendra@gmail.com>
2026-07-08 16:55:23 +0530

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,1 +1,28 @@
-// Coming Soon
+import { auth } from "./firebase-config.js";
+
+import {
+signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
+window.login = function(){
+
+const email=document.getElementById("email").value;
+const password=document.getElementById("password").value;
+
+signInWithEmailAndPassword(auth,email,password)
+
+.then(()=>{
+
+localStorage.setItem("admin","true");
+
+window.location="admin.html";
+
+})
+
+.catch(()=>{
+
+document.getElementById("msg").innerHTML="Wrong Email or Password";
+
+});
+
+}
