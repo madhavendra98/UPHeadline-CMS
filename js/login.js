766c806facbfec27115c20880b17bf9d2b0de39b
madhavendra98 <smartmadhavendra@gmail.com>
2026-07-10 11:52:14 +0530

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,1 +1,30 @@
+import { auth } from "./firebase-config.js";
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
+window.login = async function () {
+
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value;
+
+    if (!email || !password) {
+        alert("Email और Password भरें");
+        return;
+    }
+
+    try {
+
+        await signInWithEmailAndPassword(auth, email, password);
+
+        localStorage.setItem("admin", "true");
+
+        alert("✅ Login Successful");
+
+        window.location.href = "admin.html";
+
+    } catch (error) {
+
+        alert("❌ Login Failed\n\n" + error.message);
+
+    }
+
+};
